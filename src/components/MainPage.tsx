fb235e24361905cc04348632d762fca094626e1d
Rishabh Agrawal <rishabh.agrawal@getathelas.com>
2023-11-17 19:54:39 -0800

    adding css files cherry picking after resolve conflict

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,30 +1,65 @@
 // src/components/MainPage.tsx
-import React from "react";
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import React, { useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch,
+  useHistory,
+  Redirect,
+} from "react-router-dom";
 import Profile from "./Profile";
 import Activity from "./Activity";
 import NewPost from "./NewPost";
-import Feed from "./feed"; // Import Feed
+import Feed from "./Feed"; // Import Feed
+import "./MainPage.css";
 
 const MainPage = () => {
+  // const history = useHistory();
+
+  // useEffect(() => {
+  //   history.push("/activity");
+  // }, [history]);
+
   return (
     <Router>
       <div className="main-page">
-        <nav>
+        <nav className="sidebar">
           <ul>
-            <li><Link to="/activity">Activity</Link></li>
-            <li><Link to="/profile">Profile</Link></li>
-            <li><Link to="/new-post">New Post</Link></li>
-            <li><Link to="/feed">Feed</Link></li> {/* Add this line */}
+            <li>
+              <Link to="/activity">Activity</Link>
+            </li>
+            <li>
+              <Link to="/profile">Profile</Link>
+            </li>
+            <li>
+              <Link to="/new-post">New Post</Link>
+            </li>
+            <li>
+              <Link to="/feed">Feed</Link>
+            </li>
           </ul>
         </nav>
 
-        <Switch>
-          <Route path="/activity"><Activity /></Route>
-          <Route path="/profile"><Profile /></Route>
-          <Route path="/new-post"><NewPost /></Route>
-          <Route path="/feed"><Feed /></Route> {/* Add this line */}
-        </Switch>
+        <div className="content-area">
+          <Switch>
+            <Route exact path="/main">
+              <Activity />
+            </Route>
+            <Route path="/activity">
+              <Activity />
+            </Route>
+            <Route path="/profile">
+              <Profile />
+            </Route>
+            <Route path="/new-post">
+              <NewPost />
+            </Route>
+            <Route path="/feed">
+              <Feed />
+            </Route>
+          </Switch>
+        </div>
       </div>
     </Router>
   );

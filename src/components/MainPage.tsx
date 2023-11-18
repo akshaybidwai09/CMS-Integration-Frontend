f7c51f342912858c484ecaf30f4b1bc137e815bc
akshaybidwai09 <abidwai@scu.edu>
2023-11-18 00:30:27 -0800

    changing frontend

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -3,7 +3,8 @@ import React from "react";
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import Profile from "./Profile";
 import Activity from "./Activity";
-import NewPost from "./NewPost"; // Import the NewPost component
+import NewPost from "./NewPost";
+import Feed from "./feed"; // Import Feed
 
 const MainPage = () => {
   return (
@@ -11,28 +12,18 @@ const MainPage = () => {
       <div className="main-page">
         <nav>
           <ul>
-            <li>
-              <Link to="/activity">Activity</Link>
-            </li>
-            <li>
-              <Link to="/profile">Profile</Link>
-            </li>
-            <li>
-              <Link to="/new-post">New Post</Link> {/* Add this line */}
-            </li>
+            <li><Link to="/activity">Activity</Link></li>
+            <li><Link to="/profile">Profile</Link></li>
+            <li><Link to="/new-post">New Post</Link></li>
+            <li><Link to="/feed">Feed</Link></li> {/* Add this line */}
           </ul>
         </nav>
 
         <Switch>
-          <Route path="/activity">
-            <Activity />
-          </Route>
-          <Route path="/profile">
-            <Profile />
-          </Route>
-          <Route path="/new-post">
-            <NewPost /> {/* Add this line */}
-          </Route>
+          <Route path="/activity"><Activity /></Route>
+          <Route path="/profile"><Profile /></Route>
+          <Route path="/new-post"><NewPost /></Route>
+          <Route path="/feed"><Feed /></Route> {/* Add this line */}
         </Switch>
       </div>
     </Router>

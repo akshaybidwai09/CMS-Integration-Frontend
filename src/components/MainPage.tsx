b04121f8f12010d64b9410a191268d494b0d4e84
Rishabh Agrawal <rishabh.agrawal@getathelas.com>
2023-11-17 19:54:39 -0800

    adding css files

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,15 +1,29 @@
 // src/components/MainPage.tsx
-import React from "react";
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import React, { useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch,
+  useHistory,
+  Redirect,
+} from "react-router-dom";
 import Profile from "./Profile";
 import Activity from "./Activity";
 import NewPost from "./NewPost"; // Import the NewPost component
+import "./MainPage.css";
 
 const MainPage = () => {
+  // const history = useHistory();
+
+  // useEffect(() => {
+  //   history.push("/activity");
+  // }, [history]);
+
   return (
     <Router>
       <div className="main-page">
-        <nav>
+        <nav className="sidebar">
           <ul>
             <li>
               <Link to="/activity">Activity</Link>
@@ -18,22 +32,27 @@ const MainPage = () => {
               <Link to="/profile">Profile</Link>
             </li>
             <li>
-              <Link to="/new-post">New Post</Link> {/* Add this line */}
+              <Link to="/new-post">New Post</Link>
             </li>
           </ul>
         </nav>
 
-        <Switch>
-          <Route path="/activity">
-            <Activity />
-          </Route>
-          <Route path="/profile">
-            <Profile />
-          </Route>
-          <Route path="/new-post">
-            <NewPost /> {/* Add this line */}
-          </Route>
-        </Switch>
+        <div className="content-area">
+          <Switch>
+            <Route exact path="/main">
+              <Activity />
+            </Route>
+            <Route path="/activity">
+              <Activity />
+            </Route>
+            <Route path="/profile">
+              <Profile />
+            </Route>
+            <Route path="/new-post">
+              <NewPost />
+            </Route>
+          </Switch>
+        </div>
       </div>
     </Router>
   );
